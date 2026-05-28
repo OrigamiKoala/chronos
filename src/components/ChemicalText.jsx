53e4dd0e9493c69cb54ec9e11cfed07852eff2cc
Carl Liu <142947035+OrigamiKoala@users.noreply.github.com>
2026-05-27 18:32:49 -0700

    fix bugs

--- src/components/ChemicalText.jsx
+++ src/components/ChemicalText.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import SmilesDrawer from 'smiles-drawer';
 
 // Helper to determine if a token is a SMILES string
