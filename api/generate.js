a663b09238a5c067cdcf75cd2918b9fa85f85989
Carl Liu <142947035+OrigamiKoala@users.noreply.github.com>
2026-06-26 11:49:18 -0700

    oops

--- api/generate.js
+++ api/generate.js
@@ -1,5 +1,5 @@
 import { BigQuery } from '@google-cloud/bigquery';
-import { executeWithRetry as geminiRetry } from './_gemini.js';
+import { executeWithRetry } from './_gemini.js';
 
 const projectId = process.env.BIGQUERY_PROJECT_ID || 'chronos-stress-sandbox';
 
